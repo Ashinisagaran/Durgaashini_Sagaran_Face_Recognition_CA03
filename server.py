8e8c5f8579b95e434481c1061ca508ea64ef4280
Ashini Sagaran <58473849+Ashinisagaran@users.noreply.github.com>
2023-01-05 14:28:22 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from main import appRecognition
 from waitress import serve
 import socket
 
+
 sockIO = socketio.Server()
 appServer = socketio.WSGIApp(sockIO, appRecognition)
 hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+# IPAddr = socket.gethostbyname(hostname)
+IPAddr = "0.0.0.0"
 
 if __name__ == '__main__':
-    serve(appRecognition, host=IPAddr, port=8080, url_scheme='http', threads=4, log_untrusted_proxy_headers=True)
+     serve(appRecognition, host=IPAddr, port=8080, url_scheme='http', threads=4, log_untrusted_proxy_headers=True)
